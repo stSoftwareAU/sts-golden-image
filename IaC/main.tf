a28f17778d42a60236c611e283a35b8af3a1695e
Nigel Leck <Nigel.Leck@dta.gov.au>
2021-06-01 17:24:55 +1000

    Create X86 also

--- IaC/main.tf
+++ IaC/main.tf
@@ -19,6 +19,12 @@ variable "area"{
   description = "The Area"
 }
 
+variable "semanticVersion"{
+  type        = string
+  description = "The Area"
+  default="1.0.8"
+}
+
 provider "aws" {
   region=var.region
   default_tags {
@@ -30,21 +36,43 @@ provider "aws" {
 }
 
 resource "aws_imagebuilder_component" "stdPackages" {
-  data = file( "stdPackages.yaml")
+  data = replace( file( "stdPackages.yaml"),"$${REGION}", var.region)
   name = "Golden Image Standard Pacakges"
   description = "Install standard packages."
   platform = "Linux"
-  version  = "1.0.7"
+  version  = var.semanticVersion
 }
 
 data "aws_imagebuilder_component" "amazonCloudwatchAgentLinux" {
   arn = "arn:aws:imagebuilder:ap-southeast-2:aws:component/amazon-cloudwatch-agent-linux/1.0.0"
 }
 
-resource "aws_imagebuilder_image_pipeline" "golden_image" {
-  image_recipe_arn                 = aws_imagebuilder_image_recipe.golden_image.arn
+data "aws_imagebuilder_component" "updateLinux" {
+  arn = "arn:aws:imagebuilder:ap-southeast-2:aws:component/update-linux/x.x.x"
+}
+    
+data "aws_imagebuilder_component" "bootTest" {
+  arn =  "arn:aws:imagebuilder:${var.region}:aws:component/simple-boot-test-linux/1.0.0/1"
+}
+data "aws_imagebuilder_component" "rebootTest" {
+  arn = "arn:aws:imagebuilder:${var.region}:aws:component/reboot-test-linux/1.0.0/1"
+}
+
+resource "aws_imagebuilder_image_pipeline" "golden_image_arm64" {
+  image_recipe_arn                 = aws_imagebuilder_image_recipe.golden_image_arm64.arn
   infrastructure_configuration_arn = aws_imagebuilder_infrastructure_configuration.DTA_golden_image.arn
-  name                             = "Golden Image"
+  name                             = "Golden Image ARM64"
+  description = "Golden Image for launching of all other instances"
+
+  schedule {
+    schedule_expression = "cron(0 9 * * ? *)"
+  }
+}
+
+resource "aws_imagebuilder_image_pipeline" "golden_image_x86" {
+  image_recipe_arn                 = aws_imagebuilder_image_recipe.golden_image_x86.arn
+  infrastructure_configuration_arn = aws_imagebuilder_infrastructure_configuration.DTA_golden_image.arn
+  name                             = "Golden Image X86"
   description = "Golden Image for launching of all other instances"
 
   schedule {
@@ -57,11 +85,8 @@ resource "aws_imagebuilder_infrastructure_configuration" "DTA_golden_image" {
   instance_profile_name         = aws_iam_instance_profile.golden_image.name
   name                          = "DTA_golden_image"
   terminate_instance_on_failure = true
-
 }
-# data "aws_iam_policy" "AWSImageBuilderFullAccess" {
-#   arn = "arn:aws:iam::aws:policy/AWSImageBuilderFullAccess"
-# }
+
 resource "aws_iam_role" "golden_image" {
   name = "Golden-ImageV6"
   assume_role_policy  = file( "role_policy.json")
@@ -71,19 +96,12 @@ resource "aws_iam_role" "golden_image" {
   ]
 }
 
-# EC2InstanceProfileForImageBuilder, EC2InstanceProfileForImageBuilderECRContainerBuilds, and AmazonSSMManagedInstanceCore.
-
-# resource "aws_iam_role_policy_attachment" "golden_image" {
-#   role       = aws_iam_role.golden_image.name
-#   policy_arn = data.aws_iam_policy.AWSImageBuilderFullAccess.arn
-# }
-
 resource "aws_iam_instance_profile" "golden_image" {
   name = aws_iam_role.golden_image.name
   role = aws_iam_role.golden_image.name
 }
 
-resource "aws_imagebuilder_image_recipe" "golden_image" {
+resource "aws_imagebuilder_image_recipe" "golden_image_arm64" {
   block_device_mapping {
     device_name = "/dev/xvda"
 
@@ -102,8 +120,57 @@ resource "aws_imagebuilder_image_recipe" "golden_image" {
     component_arn = data.aws_imagebuilder_component.amazonCloudwatchAgentLinux.arn
   }
 
-  name         = "golden_image"
+  component {
+    component_arn = data.aws_imagebuilder_component.updateLinux.arn
+  }
+
+  component {
+    component_arn = data.aws_imagebuilder_component.bootTest.arn
+  }
+
+  component {
+    component_arn = data.aws_imagebuilder_component.rebootTest.arn
+  }
+
+  name         = "golden_image-arm64"
   parent_image = "arn:aws:imagebuilder:${var.region}:aws:image/amazon-linux-2-arm64/x.x.x"
-  version      = "1.0.6"
+  version      = var.semanticVersion
+  working_directory = "/tmp"
+}
+
+resource "aws_imagebuilder_image_recipe" "golden_image_x86" {
+  block_device_mapping {
+    device_name = "/dev/xvda"
+
+    ebs {
+      delete_on_termination = true
+      volume_size           = 8
+      volume_type           = "gp2"
+    }
+  }
+
+  component {
+    component_arn = aws_imagebuilder_component.stdPackages.arn
+  }
+
+  component {
+    component_arn = data.aws_imagebuilder_component.amazonCloudwatchAgentLinux.arn
+  }
+
+  component {
+    component_arn = data.aws_imagebuilder_component.updateLinux.arn
+  }
+
+  component {
+    component_arn = data.aws_imagebuilder_component.bootTest.arn
+  }
+
+  component {
+    component_arn = data.aws_imagebuilder_component.rebootTest.arn
+  }
+
+  name         = "golden_image-x86"
+  parent_image = "arn:aws:imagebuilder:${var.region}:aws:image/amazon-linux-2-x86/x.x.x"
+  version      = var.semanticVersion
   working_directory = "/tmp"
 }
