347ddfc2107e3f2e408a8e207e5ca9c52e4a2caa
Nigel Leck <nigel@stSoftware.com.au>
2021-10-24 08:01:00 +1100

    added SSM

--- IaC/main.tf
+++ IaC/main.tf
@@ -2,45 +2,97 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.45"
+      version = "~> 3.63"
     }
   }
 
   required_version = ">= 0.14.9"
 }
 
-variable "region"{
+variable "semanticVersion" {
+  type        = string
+  description = "The version number"
+  default     = "1.0.11"
+}
+
+/**
+ * Standard variables
+ */
+variable "area" {
+  type        = string
+  description = "The Area"
+}
+
+variable "department" {
+  type        = string
+  description = "The Department"
+}
+
+variable "region" {
   type        = string
   description = "The AWS region"
 }
 
-variable "area"{
+variable "package" {
   type        = string
-  description = "The Area"
+  description = "The Package"
+  default     = "Unknown"
 }
 
-variable "semanticVersion"{
+variable "who" {
   type        = string
-  description = "The Area"
-  default="1.0.8"
+  description = "Who did deployment"
+  default     = "Unknown"
 }
 
+variable "digest" {
+  type        = string
+  description = "The docker image Digest"
+  default     = "Unknown"
+}
+
+/* AWS provider and default tags */
 provider "aws" {
-  region=var.region
+  region = var.region
   default_tags {
     tags = {
-      Environment = var.area
-      Name        = "Golden Image"
+      Package    = var.package
+      Area       = var.area
+      Department = var.department
+      Who        = var.who
+      Digest     = var.digest
     }
   }
 }
 
+data "aws_vpc" "main" {
+
+  filter {
+    name   = "tag:Name"
+    values = ["Main"]
+  }
+}
+
+data "aws_subnet_ids" "private" {
+  vpc_id = data.aws_vpc.main.id
+
+  filter {
+    name   = "tag:Type"
+    values = ["PRIVATE"]
+  }
+}
+
+data "aws_security_group" "default" {
+  name   = "default"
+  vpc_id = data.aws_vpc.main.id
+}
+
 resource "aws_imagebuilder_component" "stdPackages" {
-  data = replace( file( "stdPackages.yaml"),"$${REGION}", var.region)
-  name = "Golden Image Standard Pacakges"
+  data        = replace(file("stdPackages.yaml"), "$${REGION}", var.region)
+  name        = "Golden Image Standard Pacakges"
   description = "Install standard packages."
-  platform = "Linux"
-  version  = var.semanticVersion
+  platform    = "Linux"
+  version     = var.semanticVersion
 }
 
 data "aws_imagebuilder_component" "amazonCloudwatchAgentLinux" {
@@ -50,9 +102,9 @@ data "aws_imagebuilder_component" "amazonCloudwatchAgentLinux" {
 data "aws_imagebuilder_component" "updateLinux" {
   arn = "arn:aws:imagebuilder:ap-southeast-2:aws:component/update-linux/x.x.x"
 }
-    
+
 data "aws_imagebuilder_component" "bootTest" {
-  arn =  "arn:aws:imagebuilder:${var.region}:aws:component/simple-boot-test-linux/1.0.0/1"
+  arn = "arn:aws:imagebuilder:${var.region}:aws:component/simple-boot-test-linux/1.0.0/1"
 }
 data "aws_imagebuilder_component" "rebootTest" {
   arn = "arn:aws:imagebuilder:${var.region}:aws:component/reboot-test-linux/1.0.0/1"
@@ -60,37 +112,39 @@ data "aws_imagebuilder_component" "rebootTest" {
 
 resource "aws_imagebuilder_image_pipeline" "golden_image_arm64" {
   image_recipe_arn                 = aws_imagebuilder_image_recipe.golden_image_arm64.arn
-  infrastructure_configuration_arn = aws_imagebuilder_infrastructure_configuration.DTA_golden_image.arn
+  infrastructure_configuration_arn = aws_imagebuilder_infrastructure_configuration.golden_image.arn
   name                             = "Golden Image ARM64"
-  description = "Golden Image for launching of all other instances"
+  description                      = "Golden Image for launching of all other instances"
 
   schedule {
-    schedule_expression = "cron(0 9 * * ? *)"
+    schedule_expression = "cron(37 2 ? * tue)"
   }
 }
 
 resource "aws_imagebuilder_image_pipeline" "golden_image_x86" {
   image_recipe_arn                 = aws_imagebuilder_image_recipe.golden_image_x86.arn
-  infrastructure_configuration_arn = aws_imagebuilder_infrastructure_configuration.DTA_golden_image.arn
+  infrastructure_configuration_arn = aws_imagebuilder_infrastructure_configuration.golden_image.arn
   name                             = "Golden Image X86"
-  description = "Golden Image for launching of all other instances"
+  description                      = "Golden Image for launching of all other instances"
 
   schedule {
-    schedule_expression = "cron(0 9 * * ? *)"
+    schedule_expression = "cron(36 2 ? * tue)"
   }
 }
 
-resource "aws_imagebuilder_infrastructure_configuration" "DTA_golden_image" {
+resource "aws_imagebuilder_infrastructure_configuration" "golden_image" {
   description                   = "Golden image"
   instance_profile_name         = aws_iam_instance_profile.golden_image.name
-  name                          = "DTA_golden_image"
+  name                          = join("-", [upper(var.department), "golden_image", lower(var.area)])
   terminate_instance_on_failure = true
+  subnet_id                     = tolist(data.aws_subnet_ids.private.ids)[0]
+  security_group_ids            = [data.aws_security_group.default.id]
 }
 
 resource "aws_iam_role" "golden_image" {
-  name = "Golden-ImageV6"
-  assume_role_policy  = file( "role_policy.json")
-  managed_policy_arns=[
+  name               = join("-", [lower(var.department), join("@", ["golden_image", lower(var.area)])])
+  assume_role_policy = file("role_policy.json")
+  managed_policy_arns = [
     "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
     "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilder"
   ]
@@ -109,6 +163,7 @@ resource "aws_imagebuilder_image_recipe" "golden_image_arm64" {
       delete_on_termination = true
       volume_size           = 8
       volume_type           = "gp2"
+      encrypted             = true
     }
   }
 
@@ -132,9 +187,9 @@ resource "aws_imagebuilder_image_recipe" "golden_image_arm64" {
     component_arn = data.aws_imagebuilder_component.rebootTest.arn
   }
 
-  name         = "golden_image-arm64"
-  parent_image = "arn:aws:imagebuilder:${var.region}:aws:image/amazon-linux-2-arm64/x.x.x"
-  version      = var.semanticVersion
+  name              = "golden_image-arm64"
+  parent_image      = "arn:aws:imagebuilder:${var.region}:aws:image/amazon-linux-2-arm64/x.x.x"
+  version           = var.semanticVersion
   working_directory = "/tmp"
 }
 
@@ -169,8 +224,8 @@ resource "aws_imagebuilder_image_recipe" "golden_image_x86" {
     component_arn = data.aws_imagebuilder_component.rebootTest.arn
   }
 
-  name         = "golden_image-x86"
-  parent_image = "arn:aws:imagebuilder:${var.region}:aws:image/amazon-linux-2-x86/x.x.x"
-  version      = var.semanticVersion
+  name              = "golden_image-x86"
+  parent_image      = "arn:aws:imagebuilder:${var.region}:aws:image/amazon-linux-2-x86/x.x.x"
+  version           = var.semanticVersion
   working_directory = "/tmp"
 }
---
+++ Jenkinsfile
@@ -0,0 +1,151 @@
+/*
+ * WARNING: Automatically copied from dga-tools
+ */
+TOOLS_IMAGE = 'dga-tools:latest'
+TOOLS_ARGS = '--volume /var/run/docker.sock:/var/run/docker.sock --volume /tmp:/tmp'
+pipeline {
+  agent none
+
+  triggers {
+    pollSCM( '* * * * *')
+    cron( 'H H(2-3) * * H(2-4)') // UTC About Midday Sydney time on a workday.
+  }
+
+  options {
+    timeout(time: 1, unit: 'HOURS')
+    disableConcurrentBuilds()
+    parallelsAlwaysFailFast()
+  }
+
+  stages {
+    stage('Build') {
+
+      when {
+        environment name: 'CHANGE_ID', value: '' // Not a Pull Request
+        beforeAgent true
+      }
+
+      agent {
+        docker {
+          image TOOLS_IMAGE
+          args TOOLS_ARGS
+        }
+      }
+
+      steps {
+        sh '''\
+          #!/bin/bash
+          set -ex
+
+          /home/tools/build.sh
+          /home/tools/push.sh
+        '''.stripIndent()
+      }
+    }
+
+    stage('QA') {
+      parallel {
+        stage('validate') {
+          when {
+            environment name: 'CHANGE_ID', value: '' // Not a Pull Request
+            beforeAgent true
+          }
+
+          agent {
+            docker {
+              image TOOLS_IMAGE
+              args TOOLS_ARGS
+            }
+          }
+          steps {
+            sh '''\
+              #!/bin/bash
+              set -ex
+
+              /home/tools/pull.sh
+              /home/tools/run.sh --require 3.7 --mode validate
+            '''.stripIndent()
+          }
+        }
+
+        stage('CVE scan') {
+          when {
+            allOf {
+              not { anyOf { branch 'Staging'; branch 'Production'; branch 'Identity'; branch 'Pipeline' } }
+              environment name: 'CHANGE_ID', value: '' // Not a Pull Request
+            }
+            beforeAgent true
+          }
+          agent {
+            docker {
+              image TOOLS_IMAGE
+              args TOOLS_ARGS
+            }
+          }
+
+          steps {
+            script {
+              try {
+                sh '''\
+                  #!/bin/bash
+                  set -ex
+
+                  /home/tools/cve-scan.sh
+                '''.stripIndent()
+                env.CVE_SCAN_FAILED = false
+              }
+              catch (err) {
+                echo "Caught: ${err}"
+                env.CVE_SCAN_FAILED = true
+              }
+            }
+          }
+          post {
+            always {
+              archiveArtifacts artifacts: 'cve-scan.json', fingerprint: true
+            }
+          }
+        }
+      }
+    }
+
+    stage('Prompt') {
+      agent none
+
+      when {
+        expression { env.CVE_SCAN_FAILED == 'true' }
+      }
+
+      steps {
+        script {
+          timeout(time: 15, unit: 'MINUTES') {
+            input( message: 'CVE scan detected issues', ok: 'Continue?')
+          }
+        }
+      }
+    }
+
+    stage('Release') {
+      when {
+        environment name: 'CHANGE_ID', value: ''
+        beforeAgent true
+      }
+
+      agent {
+        docker {
+          image TOOLS_IMAGE
+          args TOOLS_ARGS
+        }
+      }
+
+      steps {
+        sh '''\
+          #!/bin/bash
+          set -ex
+
+          /home/tools/release.sh
+        '''.stripIndent()
+      }
+    }
+  }
+}
