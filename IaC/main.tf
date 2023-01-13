481f112c0ba0b1f314c0f2d6a33d0945d80b9456
Nigel (MacBook) <nigel@laptop>
2023-01-13 18:05:07 +1100

    Fixed security group.

--- IaC/main.tf
+++ IaC/main.tf
@@ -134,7 +134,7 @@ resource "aws_imagebuilder_image_pipeline" "golden_image_x86" {
 
 resource "aws_security_group" "golden-image" {
   name        = join("-", [lower(var.department), "golden", "image", lower(var.area)])
-  description = "Aloow building of the golden images"
+  description = "Allow building of the golden images"
   vpc_id      = data.aws_vpc.main.id
   # ingress {
   #   cidr_blocks = [
@@ -156,13 +156,13 @@ resource "aws_security_group" "golden-image" {
   #   protocol    = "tcp"
   # }
 
-  # egress {
-  #   from_port        = 0
-  #   to_port          = 0
-  #   protocol         = "-1"
-  #   cidr_blocks      = ["0.0.0.0/0"]
-  #   ipv6_cidr_blocks = ["::/0"]
-  # }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   tags = {
     Name = "Golden Image"
